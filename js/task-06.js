bc436b6bf1e5af8ad49225c3bfb2b1e82dc50167
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-02-08 22:12:15 +0200

    6

---
+++ js/task-06.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', event => {
+  event.currentTarget.value.length === Number(inputEl.dataset.length)
+    ? inputEl.classList.add('valid')
+    : inputEl.classList.add('invalid');
+
+  console.log(inputEl.dataset.length);
+});
