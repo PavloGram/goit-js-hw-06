f81b195e49e12ca1febe2541139660d65c7ae2ab
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-02-09 14:58:13 +0200

    8

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,7 @@
 const inputEl = document.querySelector('#validation-input');
 
+
+
 inputEl.addEventListener('blur', event => {
   event.currentTarget.value.length === Number(inputEl.dataset.length)
     ? inputEl.classList.add('valid')
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', loginForm);
+
+function loginForm(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.email.value.length === 0 ||
+    event.currentTarget.password.value.length === 0
+  ) {
+    alert('Всі поля повинні бути заповнені!!!');
+  } else {
+    const loginInfo = {
+      password: event.currentTarget.password.value,
+      email: event.currentTarget.email.value,
+    };
+
+    console.log(loginInfo);
+
+    event.currentTarget.reset();
+  }
+}
