58370557446ca386fdbfda825fb42d823fb2e040
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-02-09 15:08:38 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorValueEl = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  colorValueEl.textContent = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
